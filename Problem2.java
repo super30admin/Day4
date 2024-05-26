d85f3ec3e280c6fd7abf92789415743add6a73bd
Tanmay-Soni <73430591+Tanmay-Soni@users.noreply.github.com>
2024-05-25 22:19:33 -0400

    Create Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,31 @@
+// Time Complexity: O(log(n))
+// Space Complexity: O(1)
+class Solution {
+    public int findMin(int[] nums) {
+        if(nums.length == 0){
+            return -1;
+        }
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low <= high){
+            if(nums[low] < nums[high]){
+                return nums[low];
+            }
+
+            int mid = low + (high - low)/2;
+
+            if((mid == 0 || nums[mid - 1] > nums[mid]) && (mid == nums.length - 1 || nums[mid + 1] > nums[mid])){
+                return nums[mid];
+            } 
+            
+            if(nums[low] <= nums[mid]){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
