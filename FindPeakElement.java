5b3328d9a3c346ba7762894a9af5d6d06124a541
Chaitanya Arava <95595265+aravachaitanya@users.noreply.github.com>
2024-07-15 14:43:46 -0500

    Update FindPeakElement.java

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -1,1 +1,23 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
 
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if((mid == 0 || nums[mid] >= nums[mid-1]) &&
+                    (mid == end || nums[mid] >= nums[mid+1])) {
+                        return mid;
+                    } else if(mid != 0 && nums[mid-1] >= nums[mid]) {
+                        end = mid - 1;
+                    } else {
+                         start = mid+1;
+                       
+                    }
+        }
+
+        return -1;
+    }
+}
