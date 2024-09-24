ae440f7b63d3813ee40986ccb3a265b385f46ce7
SriVinayA <srivinay.appari@gmail.com>
2024-09-24 04:00:48 -0700

    golang

---
+++ problem2.go
@@ -0,0 +1,25 @@
+package main
+
+func findMin(nums []int) int {
+	low, high := 0, len(nums)-1
+
+	for low <= high {
+		mid := low + (high-low)/2
+
+		//check for sorted rage
+		if nums[low] <= nums[high] {
+			return nums[low]
+		}
+
+		//check if mid is minimum
+		if (mid == 0 || nums[mid] < nums[mid-1]) && (mid == len(nums)-1 || nums[mid] < nums[mid+1]) {
+			return nums[mid]
+		} else if nums[low] <= nums[mid] { //left sorted
+			//move righ
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
