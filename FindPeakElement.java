6ac73f8451ec07c1ecd5013a026e48b399adf2c5
firoz Syed <syedfiroz2010@gmail.com>
2024-10-26 21:44:41 -0700

    Binary-Search-2 Completed

---
+++ FindPeakElement.java
@@ -0,0 +1,22 @@
+// Time Complexity:
+// Space Complexity:
+// Approach : Binary Search and move the pointers towards right or left
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if((mid==0 || nums[mid-1]<nums[mid])&&(mid == n-1 || nums[mid+1]<nums[mid])){
+                return mid;
+            }
+            else if(mid>0 && nums[mid]<nums[mid-1]){
+                high=mid-1;
+            } else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+}
