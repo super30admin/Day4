2c7ebf30364dadafeae541b1a3f7490f486075bc
Harsh <evolutionharsh@users.noreply.github.com>
2024-12-28 18:57:13 -0500

    Leetcode162.js

--- Leetcode162.js
+++ Leetcode162.js
@@ -5,17 +5,10 @@ Space complexity - Logn
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    var st= 0,end = nums.length-1;
-    while(st<end)
-    {
-        var mid1 = Math.floor((st+end)/2);
-        var mid2 = mid1+1;
-        if(nums[mid1]<nums[mid2])
-        {
-            st = mid2;
-        }
-        else
-            end = mid1;
-    }
-    return st;
+    var st=0,end = nums.length-1;
+   while(st<end){
+       var mid = Math.floor((st+end)/2);
+       (nums[mid]<nums[mid+1])?st = mid+1:end = mid;
+     }
+    return st; 
 };
