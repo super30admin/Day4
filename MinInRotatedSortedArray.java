259b08f2d90eb60585d2b4771dba700d4d386443
Tripti Ghanghas <triptighanghas23@gmail.com>
2024-12-28 23:25:17 -0800

    fixed problem 2

--- MinInRotatedSortedArray.java
+++ MinInRotatedSortedArray.java
@@ -13,7 +13,7 @@ public class MinInRotatedSortedArray {
             int mid = low+ (high-low)/2;
             if((mid == low || nums[mid] < nums[mid-1])
                     && (mid == high || nums[mid] < nums[mid+1] )){
-                return mid;
+                return nums[mid];
             }
             if(nums[low] <= nums[mid]){
                 low = mid+1;
