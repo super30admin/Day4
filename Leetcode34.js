624edba7daaf9415764326c73fb4c68cf3f7e5be
Harsh <evolutionharsh@users.noreply.github.com>
2024-12-28 19:38:12 -0500

    Add files via upload
    Binary Search -2 
    1) Leetcode 34
    2) Leetcode 153
    3) Leetocde 162

---
+++ Leetcode34.js
@@ -0,0 +1,43 @@
+/*
+Time complexity - O(logn)
+Space complexity - logn.
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    if(!nums.includes(target)){
+        return [-1,-1];
+    }
+    var left = leftside(nums,target);
+    var rgt  = rightside(nums,target);
+    return [rgt,left];
+ }
+var leftside = function(nums,target){
+    var left = -1,right=nums.length;
+    while(right-left>1){
+        var mid = Math.floor((right+left)/2);
+        if(nums[mid]<=target){
+            left = mid;
+        }else{
+            right = mid;
+        }
+    }
+    return right-1;
+}
+var rightside = function(nums,target){
+    var left=-1,right=nums.length;
+    while(right-left>1){
+        var mid = Math.floor((left+right)/2);
+        if(nums[mid]<target){
+         left = mid;
+        }
+        else{
+         right = mid;
+        }
+    }
+    return left+1;
+}
