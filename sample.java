fb82202a9a7e96ee88cf954b242ccc00c736c7af
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2024-12-28 17:54:00 -0800

    Update-2 sample.java

--- sample.java
+++ sample.java
@@ -63,3 +63,39 @@ class Solution {
         return -1;
     }
 }
+
+
+
+class Solution {
+    public int findMin(int[] nums) {
+        int low =0;
+        int high = nums.length-1;
+
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        while (low <=high){
+            if (nums[low] <= nums[high]) {
+                return nums[low];
+            }
+            int mid = low + (high-low)/2;
+            if (low == high) {
+                return nums[mid];
+            }
+            if (mid != 0 && mid != nums.length-1) {
+                if (nums[mid] < nums[mid-1] && nums[mid] < nums[mid+1]) {
+                    System.out.println("here");
+                    return nums[mid];
+                }
+            }
+            if (nums[low] <= nums[mid]) { 
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
