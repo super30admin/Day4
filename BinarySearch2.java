dad64204e0bdee98f4095c6f92e9f21af957794a
RenukaHosamani <renuhosamani@gmail.com>
2024-12-31 12:09:10 -0800

    Peak Element

--- BinarySearch2.java
+++ BinarySearch2.java
@@ -93,3 +93,32 @@ class Solution {
         return 768758;
     }
 }
+
+
+/// Find Peak Element 
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int low = 0;
+        int n = nums.length;
+        int high = n-1;
+        while (low <= high)
+        {
+            int mid = low + (high -low)/2;
+            if((mid == 0 || nums[mid] > nums[mid -1]) && (mid == n-1 || nums[mid] > nums[mid +1]))
+            {
+                return mid;
+            }
+            else if (nums[mid +1] > nums[mid]) {
+                low = mid +1;
+            }
+            else if (nums[mid -1] > nums[mid]) {
+                high = mid -1;
+            }
+
+        }
+      return -1;
+    }
+}
