d8dd6dca021b7bc37c88c05d5e79b5ad61f36283
Chaitanya7725 <chaitanya7725@gmail.com>
2024-12-31 20:06:11 -0500

    Updated the comment

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -10,7 +10,7 @@ public class FindPeakElement {
     public static void main(String[] args) {
         System.out.println(findPeakElement(new int[] { 1, 2, 3, 1 })); // 2
         System.out.println(findPeakElement(new int[] { 1, 2, 1, 3, 5, 6, 4 })); // 5
-        System.out.println(findPeakElement(new int[] { 1, 2 })); // 2
+        System.out.println(findPeakElement(new int[] { 1, 2 })); // 1
     }
 
     public static int findPeakElement(int[] nums) {
